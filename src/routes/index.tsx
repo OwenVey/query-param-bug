6e113b458b48a79eb96c2e5ff87efc2048604626
Owen Vey <owen.vey@gmail.com>
2026-06-05 11:43:55 -0500

    update deps

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,18 @@
 import { createFileRoute, Link, retainSearchParams, stripSearchParams } from '@tanstack/react-router';
 import * as v from 'valibot';
 
-export const DEFAULTS = {
+const DEFAULTS = {
   param1: 1,
   param2: 2,
-  param3: 3,
 } as const;
 
 export const Route = createFileRoute('/')({
   validateSearch: v.object({
     param1: v.optional(v.fallback(v.number(), DEFAULTS.param1), DEFAULTS.param1),
     param2: v.optional(v.fallback(v.number(), DEFAULTS.param2), DEFAULTS.param2),
-    param3: v.optional(v.fallback(v.number(), DEFAULTS.param3), DEFAULTS.param3),
   }),
   search: {
-    middlewares: [retainSearchParams(['param3']), stripSearchParams(DEFAULTS)],
+    middlewares: [retainSearchParams(['param2']), stripSearchParams(DEFAULTS)],
   },
   component: Home,
 });
@@ -29,7 +27,7 @@ function Home() {
   return (
     <div className="p-8">
       <pre>{JSON.stringify(sortedSearchParams, null, 2)}</pre>
-      <Link to={Route.path} search={{ param1: 10, param2: 20 }} style={{ display: 'block' }}>
+      <Link to={Route.path} search={{ param1: 10 }} style={{ display: 'block' }}>
         Set Search Params
       </Link>
       <Link to={Route.path} search={DEFAULTS} style={{ display: 'block' }}>
